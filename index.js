d2d9e51f65954f68049eb89fbff2d98fedda9bdc
Hassan Franco <hassan.franco@tsj.gob.mx>
2023-12-04 10:30:49 -0700

    Base de datos configurada

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
+// DB Config
+require('./database/config').dbConnection();
+
 // App de express
 const app = express();
+
+// Lectura y parseo del Body
+// app.use(express.json());
 
 // Node server
 const server = require('http').createServer(app);
@@ -15,6 +21,9 @@ require('./sockets/sockets');
 const publicPath = path.resolve(__dirname, 'public');
 
 app.use(express.static(publicPath));
+
+// Mis rutas
+// app.use('api/login', require('./routes/auth'));
 
 server.listen(process.env.PORT, (err) => {
     if (err) throw new Error(err);
